25e552b10a3cff0e2bfbe73e4e2420d0dffd990d
Enochteo <enochowoade@gmail.com>
2025-07-23 18:48:54 -0400

    add custom user creation and change form

---
+++ users/forms.py
@@ -0,0 +1,18 @@
+from  django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    class Meta:
+        model = CustomUser
+        fields = ("email","first_name","last_name")
+
+class CustomUserChangeForm(UserChangeForm ):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    class Meta:
+        model = CustomUser
+        fields = ("email","first_name","last_name")
